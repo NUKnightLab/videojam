e9535e535b2dd98d7b125a887f686803dca1b3c0
Maxine Whitely <m.whitely16@gmail.com>
2018-01-10 10:45:41 -0600

    webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const port = process.env.PORT || '8080';
 
@@ -30,15 +31,34 @@ module.exports = {
         query: {
           presets: ['es2015', 'react', 'react-hmre']
         }
+      },
+      {
+        test: /\.css/,
+        use: [
+          "style-loader",
+          "css-loader"
+        ]
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+      {
+        test: /\.png/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 12000
+            }
+          }
+        ]
       }
-      // { test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader' },
-      // {
-      //   test: /\.json$/,
-      //   loader: 'json-loader'
-      // }
     ]
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin()
   ]
 };
