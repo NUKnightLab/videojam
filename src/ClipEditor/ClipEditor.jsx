3141bdab023532a7a78507ef5c7a4cb78a1459b4
jbirori <jeffbirori@gmail.com>
2018-04-12 14:04:10 -0500

    Got the basic version of the time scrubber working. Still some work to be done, main thing now is to pass all of this data to the right places and update accordingly.

--- src/ClipEditor/ClipEditor.jsx
+++ src/ClipEditor/ClipEditor.jsx
@@ -15,11 +15,23 @@ export default class ClipEditor extends React.Component {
   		this.state = {
         mediaPath: '',
         text: 'Choose a clipcard',
+        leftScrubPos: 0,
+        rightScrubPos: 0,
+        videoStartTime : 0,
+
         }
         this.updateText = this.updateText.bind(this);
         this.scrubRight = this.scrubRight.bind(this);
         this.scrubLeft = this.scrubLeft.bind(this);
+        this.setLeftScrubPos = this.setLeftScrubPos.bind(this);
+        this.setRightScrubPos = this.setRightScrubPos.bind(this);
+        this.pausePlay = this.pausePlay.bind(this);
+        this.pauseVideo = this.pauseVideo.bind(this);
   		}
+
+      componentDidMount() {
+
+      }
 
       componentDidUpdate() {
         if (!isNaN(this.props.editorEndpoints)) {
@@ -40,27 +52,117 @@ export default class ClipEditor extends React.Component {
       }
 
       scrubRight(e) {
-        console.log( ReactDOM.findDOMNode(this.refs.container).style)
-        //document.getElementById('right-scrub').style.right
+        if (document.getElementById('editorVideo').src.split("file://")[1] == this.state.mediaPath) {
+          var container = window.getComputedStyle(ReactDOM.findDOMNode(this.refs.scrubberContainer));
+          var editorWindow = window.getComputedStyle(ReactDOM.findDOMNode(this.refs.clipEditor));
+          var leftShift = Number(editorWindow.getPropertyValue("left").split('px')[0]) + Number(container.getPropertyValue("left").split('px')[0])
+          var rightShift = leftShift + Number(container.getPropertyValue("width").split('px')[0]);
+          var rightScrubberPos = Number(window.getComputedStyle(ReactDOM.findDOMNode(this.refs.rightScrub)).getPropertyValue("right").split('px')[0]);
+          var currPos = rightShift + rightScrubberPos;
+          var mousePos = window.pageXOffset + e.clientX
+          if ((mousePos < rightShift) & (mousePos > leftShift & mousePos > this.state.leftScrubPos)) {
+            //console.log("aye whats going on", rightShift-mousePos)
+            ReactDOM.findDOMNode(this.refs.rightScrub).style.right = String(rightShift-mousePos) + "px";
+          }
+        }
+      }
+
+      setRightScrubPos() {
+        var container = window.getComputedStyle(ReactDOM.findDOMNode(this.refs.scrubberContainer));
+        var editorWindow = window.getComputedStyle(ReactDOM.findDOMNode(this.refs.clipEditor));
+        var leftShift = Number(editorWindow.getPropertyValue("left").split('px')[0]) + Number(container.getPropertyValue("left").split('px')[0]);
+        var rightShift = leftShift + Number(container.getPropertyValue("width").split('px')[0]);
+        var rightScrubberPos = Number(window.getComputedStyle(ReactDOM.findDOMNode(this.refs.rightScrub)).getPropertyValue("right").split('px')[0]);
+        var currPos = rightShift - rightScrubberPos;
+
+        var newEndTime = (Number(container.getPropertyValue("width").split('px')[0]) - rightScrubberPos)/Number(container.getPropertyValue("width").split('px')[0]);
+        var videoLength = document.getElementById('editorVideo').duration;
+        newEndTime = newEndTime * videoLength;
+        console.log(newEndTime);
+
+        document.getElementById('editorVideo').currentTime = this.state.videoStartTime;
+
+        this.setState({
+          'rightScrubPos' : currPos,
+          'endTime' : newEndTime,
+        });
       }
 
       scrubLeft(e) {
-        console.log(window.getComputedStyle(ReactDOM.findDOMNode(this.refs.leftScrub)).getPropertyValue("left"))
-        var container = window.getComputedStyle(ReactDOM.findDOMNode(this.refs.scrubberContainer))
-        var editorWindow = window.getComputedStyle(ReactDOM.findDOMNode(this.refs.clipEditor))
-        var leftShift = Number(editorWindow.getPropertyValue("left").split('px')[0]) + Number(container.getPropertyValue("left").split('px')[0]);
-        var currPos = leftShift + Number(window.getComputedStyle(ReactDOM.findDOMNode(this.refs.leftScrub)).getPropertyValue("left").split('px')[0]);
-        console.log(currPos, e.clientX)
+        if (document.getElementById('editorVideo').src.split("file://")[1] == this.state.mediaPath) {
+          var container = window.getComputedStyle(ReactDOM.findDOMNode(this.refs.scrubberContainer));
+          var editorWindow = window.getComputedStyle(ReactDOM.findDOMNode(this.refs.clipEditor));
+          //console.log("ayo is this px or %", editorWindow.getPropertyValue("left").split('px')[0])
+          var leftShift = Number(editorWindow.getPropertyValue("left").split('px')[0]) + Number(container.getPropertyValue("left").split('px')[0]);
+          var rightShift = leftShift + Number(container.getPropertyValue("width").split('px')[0]);
+          var leftScrubberPos = Number(window.getComputedStyle(ReactDOM.findDOMNode(this.refs.leftScrub)).getPropertyValue("left").split('px')[0]);
+          var currPos = leftShift + leftScrubberPos;
+          var mousePos = window.pageXOffset + e.clientX
+          var stateRightPos = this.state.rightScrubPos;
+          if (stateRightPos == 0) {
+            stateRightPos = rightShift
+          }
+          //console.log(currPos, window.pageXOffset + e.clientX, leftShift, leftScrubberPos)
+          if (mousePos > leftShift & (mousePos < rightShift & mousePos < stateRightPos)) { //GET STATE.RIGHTSCRUBPOS TO NOT BE 0 INITIALLY!
+            ReactDOM.findDOMNode(this.refs.leftScrub).style.left = String(mousePos-leftShift) + "px";
+          }
+        }
+      }
 
+      setLeftScrubPos() {
+        var container = window.getComputedStyle(ReactDOM.findDOMNode(this.refs.scrubberContainer));
+        var editorWindow = window.getComputedStyle(ReactDOM.findDOMNode(this.refs.clipEditor));
+        var leftShift = Number(editorWindow.getPropertyValue("left").split('px')[0]) + Number(container.getPropertyValue("left").split('px')[0]);
+        var leftScrubberPos = Number(window.getComputedStyle(ReactDOM.findDOMNode(this.refs.leftScrub)).getPropertyValue("left").split('px')[0]);
+
+        var newStartTime = leftScrubberPos/Number(container.getPropertyValue("width").split('px')[0])
+        var videoLength = document.getElementById('editorVideo').duration
+        newStartTime = newStartTime * videoLength;
+        document.getElementById('editorVideo').currentTime = newStartTime
+        var currPos = leftShift + leftScrubberPos;
+        this.setState({
+          'leftScrubPos' : currPos,
+          'videoStartTime' : newStartTime,
+        });
+      }
+
+      pausePlay() {
+        if (document.getElementById('editorVideo').src.split("file://")[1] == this.state.mediaPath) {
+          var button = document.getElementById('play-pause-button')
+          var video = document.getElementById('editorVideo')
+          if (button.innerHTML == "►") {
+            button.innerHTML = "&#10074;&#10074;"
+            console.log()
+            if (video.currentTime == video.duration || video.currentTime == 0 || (video.currentTime <= (this.state.endTime + 0.15) & video.currentTime >= (this.state.endTime - 0.15))) {
+              video.currentTime = this.state.videoStartTime;
+            }
+            video.play();
+          }
+          else {
+            button.innerHTML = "&#9658;"
+            video.pause();
+          }
+        }
+      }
+
+      videoDone() {
+        document.getElementById('play-pause-button').innerHTML = "&#9658;"
+      }
+
+      pauseVideo() {
+        var video = document.getElementById('editorVideo')
+        console.log(video.currentTime, this.state.endTime, video.currentTime <= (this.state.endTime + 0.2) & video.currentTime >= (this.state.endTime - 0.2))
+        if (video.currentTime <= (this.state.endTime + 0.15) & video.currentTime >= (this.state.endTime - 0.15)) {
+          video.pause()
+        }
       }
 
   render() {
-    const rightScrub = {right: '1%'}
     return (
       <div ref={"clipEditor"} className="clipEditor">
         <div id="clipEdit-container">
           <div>
-            <video id="editorVideo" width="440" height="360" src={this.state.mediaPath} controls></video>
+            <video id="editorVideo" width="440" onTimeUpdate={this.pauseVideo} onPause={this.videoDone} height="360" src={this.state.mediaPath} controls></video>
           </div>
           <textarea
             id="editorText"
@@ -70,8 +172,9 @@ export default class ClipEditor extends React.Component {
         </div>
         <div ref={"scrubberContainer"} id="scrubber-container">
           <div id="scrubber-line"></div>
-          <div onClick={this.scrubLeft} ref={"leftScrub"} id="left-scrub"></div>
-          <div onClick={this.scrubRight} style={rightScrub} id="right-scrub"></div>
+          <p id="play-pause-button" onClick={this.pausePlay}> &#9658; </p>
+          <div onDrag={this.scrubLeft} onDragEnd={this.setLeftScrubPos} ref={"leftScrub"} id="left-scrub" draggable></div>
+          <div onDrag={this.scrubRight} onDragEnd={this.setRightScrubPos} ref={"rightScrub"} id="right-scrub" draggable></div>
         </div>
 
       </div>
